970edae28cb52fd94969683e4e9289bac773c4f7
Clinton Moshe <clintonmwachia9@gmail.com>
2025-01-12 22:41:53 +0300

    added update inputs

---
+++ 11.0 Dynamic UI/1.0 Updating Inputs.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+ui <- fluidPage(
+  numericInput("min", "Minimum", 0),
+  numericInput("max", "Maximum", 3),
+  sliderInput("n", "n", min = 0, max = 3, value = 1)
+)
+
+# when you update the minimum value, the minimum for the slider input
+# is updated to the value of input id=min,
+
+# the same behaviour is observed on the maximum.
+
+server <- function(input, output, session) {
+  observeEvent(input$min, {
+    updateSliderInput(inputId = "n", min = input$min)
+  })
+  observeEvent(input$max, {
+    updateSliderInput(inputId = "n", max = input$max)
+  })
+}
+
+shinyApp(ui, server)
